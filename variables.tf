2cb5240f303bd70bd2828bbcbfea4c9cfe32d7f8
Seenu <sreenivas.sree.durgam@gmail.com>
2020-01-12 22:41:11 +0530

    prasa_rg

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   type        = "string"
   description = "Name of the azure resource group."
-  default = "vijay_rg"
+  default = "prasa_rg"
 }
 
 variable "resource_group_location" {
