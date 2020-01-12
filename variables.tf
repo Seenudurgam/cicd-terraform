07830f9e96cbff1001512ad971f55721c30d39ce
Seenu <sreenivas.sree.durgam@gmail.com>
2020-01-12 23:02:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,35 +10,7 @@ variable "resource_group_location" {
   default = "westeurope"
 }
 
-variable "subscription_id" {
-  type        = "string"
-  description = "subscription ID."
-  default = "9657d952-ba4f-4588-8ada-bf25a4524929"
-}
 
-variable "client_id" {
-  type        = "string"
-  description = "client ID"
-  default = "a044fb05-dff5-4101-a571-3082d2bb761f"
-}
-
-variable "client_secret" {
-  type        = "string"
-  description = "client secret"
-  default = "lWbltKXG_8e.9baX-sKnT1r4GvCK/atJ"
-}
-
-variable "tenant_id" {
-  type        = "string"
-  description = "tenant id"
-  default = "ac67e308-1f19-4011-9baa-c4df0351e262"
-}
-
-variable "object_id" {
-  type        = "string"
-  description = "object id"
-  default = "1c584611-915e-4307-a62a-36f4ff1828fd"
-}
 /*
 variable "appinsight" {
   type        = "string"
