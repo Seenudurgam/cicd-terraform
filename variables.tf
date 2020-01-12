8f5f59bdb1ae767f44d2741b115adbacbda45f5c
Seenu <sreenivas.sree.durgam@gmail.com>
2020-01-12 20:52:40 +0530

    durgams_rg

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   type        = "string"
   description = "Name of the azure resource group."
-  default = "durgam_rg"
+  default = "durgams_rg"
 }
 
 variable "resource_group_location" {
