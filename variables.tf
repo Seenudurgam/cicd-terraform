c6bedf68e08484e8b5eedc0669ce7475f0545d12
Seenu <sreenivas.sree.durgam@gmail.com>
2020-01-16 11:42:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   type        = "string"
   description = "Name of the azure resource group."
-  default = "harshaa_rg"
+  default = "mdrdi_rg"
 }
 
 variable "resource_group_location" {
