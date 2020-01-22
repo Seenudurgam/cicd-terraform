573b1540c5f38da0e8bf3b33284d2d90251ee56a
Seenu <sreenivas.sree.durgam@gmail.com>
2020-01-22 13:32:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,83 +1,40 @@
 variable "resource_group_name" {
   type        = string
-  description = "Resource group name that will contain various resources"
-  default = "mmech-ugm-dev-amap-rg"
+  description = "Name of the azure resource group."
+  default = "seenu"
 }
-
-variable "resource_group_name_common" {
-  type        = string
-  description = "Resource group name that will contain various resources"
-  default = "mmech-ugm-dev-common-amap-rg"
-}
-
 variable "resource_group_location" {
   type        = string
-  description = "The default Azure region for the resource provisioning"
-  default = "Central US"
+  description = "Location of the azure resource group."
+  default = "westeurope"
 }
 
 variable "subscription_id" {
   type        = string
-  description = "Enter Subscription ID for provisioning resources in Azure."
-  default = "XYXYX-SubscriptionID-XYXYX"
+  description = "subscription ID."
+  default = "9657d952-ba4f-4588-8ada-bf25a4524929"
 }
 
 variable "client_id" {
   type        = string
-  description = "Enter Client ID for Application created in Azure AD"
-  default = "XYXYX-ClientID-XYXYX"
+  description = "client ID"
+  default = "a044fb05-dff5-4101-a571-3082d2bb761f"
 }
 
 variable "client_secret" {
   type        = string
-  description = "Enter Client secret for Application in Azure AD"
-  default = "XYXYX-ClientSecret-XYXYX"
+  description = "client secret"
+  default = "lWbltKXG_8e.9baX-sKnT1r4GvCK/atJ"
 }
 
 variable "tenant_id" {
   type        = string
-  description = "Enter Tenant ID / Directory ID of your Azure AD. Run Get-AzureSubscription to know your Tenant ID"
-  default = "XYXYX-TenantID-XYXYX"
+  description = "tenant id"
+  default = "ac67e308-1f19-4011-9baa-c4df0351e262"
 }
 
 variable "object_id" {
   type        = string
-  description = "Enter Object ID of a user or a service principal in Azure Active Directory"
-  default = "XYXYX-ObjectID-XYXYX"
-}
-
-/*variable "appinsight" {
-  type        = "string"
-  description = "Enter the Application insight name"
-  default = "mmech-ugmappinsights-dev-amap"
-}*/
-
-variable "storageName" {
-  type        = string
-  description = "Enter the Storage Account name"
-  default = "mmechugmstrgacdevamap (Unique)"
-}
-
-variable "sqlserver" {
-  type        = string
-  description = "Enter the Sql Server name"
-  default = "mmech-ugmsqlsrv-dev-amap"
-}
-
-variable "db" {
-  type        = string
-  description = "Enter the Data base name"
-  default = "mmech-ugmdb-dev-amap"
-}
-
-variable "kv" {
-  type        = string
-  description = "Enter the Key Vault name"
-  default = "mmech-ugmkv-dev-amap"
-}
-
-variable "acr" {
-  type        = string
-  description = "Enter the Container Registry name (Unique)"
-  default = "mmechugmacrdevamap"
+  description = "object id"
+  default = "1c584611-915e-4307-a62a-36f4ff1828fd"
 }
